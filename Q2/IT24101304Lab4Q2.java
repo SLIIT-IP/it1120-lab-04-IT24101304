ed179a9dc09061e6316b415116cf0ce0f78acc1a
IT24101304 <IT24101304@my.sliit.lk>
2024-08-21 10:46:28 +0530

    Add Lab4Q2 answers Q2 folders

--- Q2/IT24101304Lab4Q2.java
+++ Q2/IT24101304Lab4Q2.java
@@ -5,36 +5,30 @@ public class IT24101304Lab4Q2 {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Please enter exam marks (out of 100) : ");
-        int ExamMarks = scanner.nextInt();
-        if (ExamMarks > 100 || ExamMarks < 0) {
+        int Exam_Marks = scanner.nextInt();
+        if (Exam_Marks > 100 || Exam_Marks < 0) {
             System.out.println("Invalid input for exam marks. Terminating program.");
             return;
         }
         System.out.println("Please enter exam lab submission marks (out of 100) : ");
-        int labMarks = scanner.nextInt();
-        if (labMarks > 100 || labMarks < 0) {
+        int Lab_Marks = scanner.nextInt();
+        if (Lab_Marks > 100 || Lab_Marks < 0) {
             System.out.println("Invalid input for lab submission marks. Terminating program.");
             return;
         }
         System.out.println("Please enter the percentage given for the exam : ");
-        int ExamPercent = scanner.nextInt();
-        if (ExamPercent > 100 || ExamPercent < 0) {
-            return;
-        }
+        int Exam_Percent = scanner.nextInt();
         System.out.println("Please enter the percentage given for the lab submission : ");
-        int LabPercent = scanner.nextInt();
-        if (LabPercent > 100 || LabPercent < 0) {
-            return;
-        }
+        int Lab_Percent = scanner.nextInt();
 
-        int Exam = ExamMarks * ExamPercent / 100;
-        int Lab = labMarks * LabPercent / 100;
+        int Exam = Exam_Marks * Exam_Percent / 100;
+        int Lab = Lab_Marks * Lab_Percent / 100;
 
-        if (LabPercent + ExamPercent != 100) {
+        if (Lab_Percent + Exam_Percent != 100) {
             System.out.println("The Percentages must add up to 100. Terminating program.");
             return;
         }
-        double Final = Exam + Lab;
-            System.out.println("Final mark is : " +Final);
+        double Final_Marks = Exam + Lab;
+        System.out.println("Final mark is : " + Final_Marks);
     }
 }
